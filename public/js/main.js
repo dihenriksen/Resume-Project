f92e276b06571be2710ab819ad946af2f9d2bb2b
Web Development <webdevelopment@Daniels-MacBook-Air.local>
2013-06-28 12:45:22 -0700

    Split .js into two files. Finish JSON data formatting

--- public/js/main.js
+++ public/js/main.js
@@ -1,118 +1,106 @@
-// $(document).ready(function() {
-// 	console.log('with a little jiggery-pokery...');
-// 	$.ajax('/api/resumes/51c20838496a9d577d000001',{
-// 		complete : function(response) {
-// 			console.log(response.responseJSON);
-// 			$('#first_name').html(response.responseJSON.name_first);
-// 			if (response.responseJSON.name_middle) {
-// 				$('#middle_name').html(response.responseJSON.name_middle);
-// 			} else {
-// 				$('#middle_name').html(response.responseJSON.name_middle);
-// 			}
-// 			$('#last_name').html(response.responseJSON.name_last);
-// 			$('#street').html(response.responseJSON.contact_info.street_address.street);
-// 			city = response.responseJSON.contact_info.street_address.city;
-// 			city_proper = city[0];
-// 			for (i = 1; i < city.length; i++) {
-// 				if (city[i] == city[i].toLowerCase()) {
-// 					city_proper += city[i];
-// 				} else {
-// 					city_proper += ' ' + city[i];
-// 				}
-// 			};
-
-
-// 			state = response.responseJSON.contact_info.street_address.state;
-// 			zip = response.responseJSON.contact_info.street_address.zip_code;
-// 			cityStateZip = city_proper + ', ' + state + ', ' + zip;
-// 			$('#cityStateZip').html(cityStateZip);
-// 			$('#email').html(response.responseJSON.contact_info.email);
-
-
-// 			phone_number = response.responseJSON.contact_info.phone;
-// 			phone = '';
-// 			for (i = 0; i < phone_number.length; i++) {
-// 				x = phone_number[i];
-// 				if (phone.length == 0) {
-// 					phone += '(';
-// 				}
-// 				if (phone.length == 4) {
-// 					phone += ') ';
-// 				}
-// 				if (phone.length == 9) {
-// 					phone += '-';
-// 				}
-// 				if (x == 0 || x == 1 || x == 2 || x == 3 || x == 4 || x == 5 || x == 6 || x == 7 || x == 8 || x == 9) {
-// 					phone += phone_number[i];
-// 				}
-// 				if (phone.length == 14) {
-// 					break;
-// 				}
-// 			}
-// 			$('#phone').html(phone);
-
-
-// 			$('#linked_in').html(response.responseJSON.linked_in);
-// 			full_name = response.responseJSON.name_first + ' ' + response.responseJSON.name_last;
-// 			website = '<a href="' + response.responseJSON.website +'">' + full_name + '</a>';
-// 			$('#website').html(website);
-
-
-// 			skill_type = response.responseJSON.skill[0].category;
-// 			$('.skills ul:first-child').html(skill_type);
-// 		}
-// 	});
-// });
-
-
 $(document).ready(function() {
+	$.ajax('/api/resumes', {
+		complete : function(response) {
+			var resume = response.responseJSON[0];
+		}
+	});
 
 	$('.education_block_add').click(add_education);
-
 	$('.category_block_add').click(add_category);
-
 	$('.skill_block_add').click(add_skill);
-
 	$('.work_block_add').click(add_work);
+	$('.greatness_block_add').click(add_greatness);
+	$('.responsibility_add').click(add_responsibility);
 
 	$('#user_data').submit(function() {
 		var userData 		= {};
 		userData.name_first = $('#name_first').val();
 		userData.name_last 	= $('#name_last').val();
-		userData.phone 		= $('#phone').val();
-		userData.email 		= $('#email').val();
-		userData.twitter 	= $('#twitter').val();
-		userData.site 		= $('#personal_site').val();
-		userData.address 	= $('#address').val();
-		userData.city 		= $('#city').val();
-		userData.state 		= $('#state').val();
-		userData.zip 		= $('#zip').val();
 
-		userData.skills = [];
+		userData.contact_info = {
+			email : $('#email').val(),
+			phone : $('#phone').val(),
+			street_address : {
+				city 	 : $('#city').val(),
+				state 	 : $('#state').val(),
+				street 	 : $('#street').val(),
+				zip_code : $('#zip').val()
+			}
+		};
+
+		userData.twitter 	= $('#twitter').val();
+		userData.website 	= $('#personal_site').val();
+		userData.linked_in	= $('#linked_in').val();
+
+		userData.experience = [];
+		$('.work_experience').find('.work_block').each(function(index, element) {
+			if ($(element).find('.company').val()) {
+				var responsibilities = [];
+				$(element).find('.responsibilities').each(function(index1, element1) {
+					responsibilities.push($(element1).val());
+				});
+
+				userData.experience.push({
+					end_month_year 		: $(element).find('.work_end').val(),
+					location 			: $(element).find('.location').val(),
+					organization 		: $(element).find('.company').val(),
+					project 			: $(element).find('.project').val(),
+					role 				: $(element).find('.position').val(),
+					start_month_year 	: $(element).find('.work_start').val(),
+					responsibilities 	: responsibilities
+				});
+			}
+		});
+
+		userData.skill = [];
 		$('.skills').find('.skill').each(function(index, element) {
-			if ($(element).val() != '') {
-				userData.skills.push({
-					skill 		: $(element).val(),
+			if ($(element).val()) {
+				userData.skill.push({
+					title 		: $(element).val(),
 					experience 	: $(element).parent().next().children('.years_experience').val(),
 					category 	: $(element).closest('.skills_container').prev().find('.category').val()
 				});
 			};
-			
 		});
-		console.log(userData.skills); 
 
-		userData.education = [];
+		userData.schools = [];
 		$('.education_block').each(function(index, element) {
-			userData.education.push({
-				school		: $(element).find('.school').val(),
-				degree		: $(element).find('.degree').val(),
-				major		: $(element).find('.major').val(),
-				minor		: $(element).find('.minor').val(),
-				gpa			: $(element).find('.gpa').val(),
-				societies 	: $(element).find('.secret_societies').val(),
-				graduation 	: $(element).find('.graduation_date').val()
-			})
+			if ($(element).find('.school').val()) {
+				userData.education.push({
+					name		: $(element).find('.school').val(),
+					degree		: $(element).find('.degree').val(),
+					major		: $(element).find('.major').val(),
+					minor		: $(element).find('.minor').val(),
+					gpa			: $(element).find('.gpa').val(),
+					// societies 	: $(element).find('.secret_societies').val(),
+					end_month_year 	: $(element).find('.graduation_date').val(),
+					start_month_year : $(element).find('.matriculation_date').val()
+				});
+			}
+		});
+
+		userData.accomplishments = [];
+		$('.accomplishments_block').each(function(index, element) {
+			if ($(element).find('.title').val()) {
+				userData.accomplishments.push({
+					title 		: $(element).find('.title').val(),
+					month_year 	: $(element).find('.accomplishment_date').val(),
+					description : $(element).find('.description').val()
+				});
+			}
+		});
+		console.log(userData);
+
+
+		var the_data = JSON.stringify({ 'resume' : userData});
+		console.log(the_data);
+		$.ajax({
+			type : 'POST',
+			url : '/api/resumes',
+			data : the_data
 		})
+
+
 
 
 		console.log('The end of the submit function');
@@ -141,19 +129,6 @@ function add_category() {
 	return false;
 }
 
-function add_work() {
-	var html = $('.work_block').first().clone();
-	html.css('display', 'none');
-	$(this).parent().before(html);
-	html.find($('.company')).val('');
-	html.find($('.position')).val('');
-	html.find($('.work_start')).val('');
-	html.find($('.work_end')).val('');
-	html.find($('.responsibilities')).val('');
-	html.slideDown(500);
-	return false;
-}
-
 function add_education() {
 	var html = $('.education_block').first().clone();
 	html.css('display', 'none');
@@ -169,9 +144,47 @@ function add_education() {
 	return false;
 }
 
+function add_greatness() {
+	var html = $('.accomplishments_block').first().clone();
+	html.css('display', 'none');
+	$(this).parent().before(html);
+	html.find('.title').val('');
+	html.find('.accomplishment_date').val('');
+	html.find('.description').val('');
+	html.slideDown(500);
+	return false;
+}
 
+function add_responsibility() {
+	var html = $('.resp').first().clone();
+	html.css({
+		'display' : 'none',
+		'margin-top' : '20px'
+	});
+	console.log('now here');
+	$(this).parent().before(html);
+	html.find('.responsibilities').val('');
+	html.slideDown(500);
+	return false;
+}
 
-
+function add_work() {
+	var html = $('.work_block').first().clone();
+	var button = html.find('.btn');
+	button.click(add_responsibility);
+	html.find('.resp').slice(1).remove();
+	html.css('display', 'none');
+	$(this).parent().before(html);
+	html.find($('.company')).val('');
+	html.find($('.location')).val('');
+	html.find($('.position')).val('');
+	html.find($('.work_start')).val('');
+	html.find($('.work_end')).val('');
+	html.find($('.project')).val('');
+	html.find($('.responsibilities')).val('');
+	html.slideDown(500);
+	return false;
+}
 
 
 
